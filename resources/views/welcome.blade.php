ca944f0f3dffff64f6b7e17892fffbb6a95de5ff
krls2020 <mail@karelhemza.cz>
2024-12-04 10:45:42 +0100

    fix dark mode styling for sessions card

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -57,15 +57,14 @@
                             </nav>
                         @endif
                     </header>
-
                     <div class="mt-16">
                         <div class="grid grid-cols-1 gap-6 lg:gap-8">
-                            <div class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex">
+                            <div class="scale-100 p-6 bg-white dark:bg-zinc-900 rounded-lg shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] dark:ring-zinc-800 flex motion-safe:hover:scale-[1.01] transition-all duration-250">
                                 <div>
-                                    <h2 class="text-xl font-semibold text-gray-900 dark:text-white">
+                                    <h2 class="text-xl font-semibold text-black dark:text-white">
                                         Active Sessions
                                     </h2>
-                                    <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+                                    <p class="mt-4 text-sm/relaxed text-black/70 dark:text-white/50">
                                         Current number of active sessions in database: {{ $sessionsCount }}
                                     </p>
                                 </div>
